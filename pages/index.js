75cb3c18eadf4cb5420f72bdfae08a4da7c62f4a
Gabriel Borges <164194935+contattoGabriel@users.noreply.github.com>
2024-09-17 15:28:51 +0000

    teste para imagem da Fundação do Site

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-  return <h1>Sabrine, eu amo você. Se você me ama, da uma risadinha!🧐 </h1>;
+  <h1>Sabrine, eu amo você. Se você me ama, da uma risadinha!🧐 </h1>;
+  <img
+    src="assets/img/WhatsApp Image 2024-09-17 at 12.04.18.jpeg"
+    alt="Logo DocNews"
+  ></img>;
+  return;
 }
 
 export default Home;
