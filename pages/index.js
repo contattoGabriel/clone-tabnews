1568d2f95f100ff27d508ea343507c4aff4ce107
Gabriel Borges <164194935+contattoGabriel@users.noreply.github.com>
2024-09-17 17:44:34 +0000

    logo docnews

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,7 @@ function Home() {
   return (
     <div>
       <h1>Em breve, um novo acervo de documentação Empresarial</h1>
-      <img
-        src="assets/img/WhatsApp Image 2024-09-17 at 12.04.18.jpeg"
-        alt="Logo DocNews"
-      ></img>
+      <img src="assets/img/logo.jpeg" alt="Logo DocNews"></img>
     </div>
   );
 }
