9a1f0b8aa6a8ce7cd94c90cb408f29073e1bbf64
Gabriel Borges <164194935+contattoGabriel@users.noreply.github.com>
2024-09-17 19:31:00 +0000

    teste imagem 2

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ function Home() {
   return (
     <div>
       <h1>Em breve, um novo acervo de documentação Empresarial</h1>
-      <img src="/public/logo.jpeg" alt="Logo DocNews" />
+      <img src="/logo.jpeg" alt="Logo DocNews"/>
     </div>
   );
 }
